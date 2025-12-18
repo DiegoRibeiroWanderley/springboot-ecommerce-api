64e0c0c3eac054d0000f8f46f8d9c150d2cec92e
Diego <eliwandejp@gmail.com>
2025-12-17 21:43:35 -0300

    JPA @Entity and @Id

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,8 +1,16 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
     private Long categoryId;
     private String categoryName;
+
+    public Category() {
+    }
 
     public Category(Long id, String categoryName) {
         this.categoryId = id;
