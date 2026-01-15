95d9ce261366bde4f29e0107c3c0c9a10b301de6
Diego <eliwandejp@gmail.com>
2026-01-14 23:51:46 -0300

    Create CartMapper with MapStruct

---
+++ src/main/java/com/ecommerce/project/mapper/CartMapper.java
@@ -0,0 +1,12 @@
+package com.ecommerce.project.mapper;
+
+import com.ecommerce.project.model.Cart;
+import com.ecommerce.project.payload.CartDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface CartMapper {
+    @Mapping(target = "products", ignore = true)
+    CartDTO toDTO(Cart cart);
+}
